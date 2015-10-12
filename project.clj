4b28e0b4ef8a4c2f123b09e70ae2328c1a77098a
Eric Chaves <eric@craftti.com.br>
2015-10-12 00:07:29 -0300

    reorganized get-code function

--- project.clj
+++ project.clj
@@ -5,27 +5,31 @@
                  [org.clojure/clojurescript "0.0-3308"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [io.nervous/cljs-lambda "0.1.2"]]
+
   :plugins [[lein-cljsbuild "1.0.6"]
             [lein-npm "0.5.0"]
             [io.nervous/lein-cljs-lambda "0.2.4"]]
+
   :node-dependencies [[source-map-support "0.2.8"]
                       [gm "1.9.0"]]
-  :profiles
-  { :dev { :plugins [[org.bodil/lein-noderepl "0.1.11"]] }}
+  :profiles { :dev { :plugins [[org.bodil/lein-noderepl "0.1.11"]] }}
+
   :source-paths ["src"]
-  :cljs-lambda
-  {:cljs-build-id "simple-aws-captcha"
+
+  :cljs-lambda {
+   :cljs-build-id "simple-aws-captcha"
    :aws-profile "default"
    :defaults {:role "arn:aws:iam::FIXME:role/cljs-lambda-default", :create true}
    :functions
    [{:name   "simple-captcha"
      :invoke simple-aws-captcha.core/get-captcha
      :description "create captcha and store it on memcached returning its image"}]}
-  :cljsbuild
-  {:builds [{:id "simple-aws-captcha"
-             :source-paths ["src"]
-             :compiler {:output-to "out/simple_aws_captcha.js"
-                        :output-dir "out"
-                        :target :nodejs
-                        :optimizations :none
-                        :source-map true}}]})
+
+  :cljsbuild {
+     :builds [{:id "simple-aws-captcha"
+               :source-paths ["src"]
+               :compiler {:output-to "out/simple_aws_captcha.js"
+                          :output-dir "out"
+                          :target :nodejs
+                          :optimizations :none
+                          :source-map true}}]})
--- src/simple_aws_captcha/core.cljs
+++ src/simple_aws_captcha/core.cljs
@@ -2,11 +2,12 @@
   (:require [cljs-lambda.util :refer [async-lambda-fn]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn get-code [x]
-  (take x (shuffle (seq "abcdefghijklmnopqrstuvwxyz0123456789"))))
+(defn get-code
+  ([] (take 4 (shuffle (seq "abcdefghijklmnopqrstuvwxyz0123456789"))))
+  ([len] (take len (shuffle (seq "abcdefghijklmnopqrstuvwxyz0123456789")))))
 
 (def ^:export get-captcha
   (async-lambda-fn
-   (fn [{:keys [variety]} context]
+   (fn [{:keys [code]} context]
      (go
        (hash-map :code (get-code 4))))))
