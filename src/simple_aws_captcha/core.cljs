b429a668186eb304f2412718dea188ad5cfabbda
Eric Chaves <eric@craftti.com.br>
2015-10-18 17:55:54 -0200

    trying to call child_process from cljs (not working yet)

--- src/simple_aws_captcha/core.cljs
+++ src/simple_aws_captcha/core.cljs
@@ -1,10 +1,28 @@
 (ns simple-aws-captcha.core
-  (:require [cljs-lambda.util :refer [async-lambda-fn]])
+  (:require [cljs-lambda.util :refer [async-lambda-fn]]
+            [cljs.nodejs :as node]
+            [cljs.core.async :as async])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
+(defn exec [cmd]
+  (let [ch (async/chan)
+        ps (node/require "child_process")]
+    (.exec ps cmd (fn [err stdout]
+                    (if (nil? err)
+                      (async/put! ch stdout)
+                      (async/put! ch "error")
+                      )))
+    ch))
+
 (defn get-code
-  ([] (take 4 (shuffle (seq "abcdefghijklmnopqrstuvwxyz0123456789"))))
-  ([len] (take len (shuffle (seq "abcdefghijklmnopqrstuvwxyz0123456789")))))
+  "returns a random code sequence"
+  [& {:keys [size chars]
+      :or {size 4 chars "abcdhknrstuvxz23456789"}}]
+  (take size (shuffle (seq chars))))
+
+(defn get-image
+  "returns png image for a given code"
+  ([code] true))
 
 (def ^:export get-captcha
   (async-lambda-fn
--- test/simple_aws_captcha/core_test.cljs
+++ test/simple_aws_captcha/core_test.cljs
@@ -1,9 +1,42 @@
 (ns simple-aws-captcha.core-test
+  (:require-macros [cljs.core.async.macros :refer [go alt!]])
   (:require [cljs.test :refer-macros [deftest is testing async run-tests]]
-            [simple-aws-captcha.core :refer [get-code]]))
+            [cljs.core.async :refer [>! <! chan put! take! timeout close!]]
+            [simple-aws-captcha.core :refer [get-code get-image exec]]))
 
 (enable-console-print!)
 
 (deftest smoking-test
   (testing "should work"
     (is (= true true))))
+
+(deftest get-code-function
+  (testing "accepts a size for the returned code"
+    (let [code (get-code :size 6)]
+      (is (= 6 (count code)))))
+
+  (testing "accepts a string with valid code chars"
+    (let [code (get-code :chars "1111")]
+      (is (= '("1" "1" "1" "1") code))))
+  
+  (testing "returns a random seq of chars"
+    (let [code (get-code)]
+      (is (= 4 (count code)))
+      (is (seq? code)))))
+
+(deftest get-image-function
+  (testing "accepts a chars seq"
+    (let [code [\a \b \c \d]]
+      (is (get-image code))))
+  
+  (testing "returns image from a chars seq"
+    (is true)))
+
+(deftest call-convert-with-exec
+  (testing "trying to exec a simple cmd command"
+    (let [ch (exec "ls -al /tmp")]
+      (print "so far so good")
+      (go
+       (<! ch)
+       (print "end of go block")))))
+
